9962b8739eb408da26b04a38575b60b61bba261c
Cristiano Costa <engcristianoc@gmail.com>
2016-10-21 17:15:56 -0200

    Adicionando página de login

--- forno/urls.py
+++ forno/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from usuarios.views import Usuario
+from usuarios.views import Usuario, Auth
 from forno.settings import DEBUG
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', Usuario.as_view()),
+    url(r'^login/', Auth.login, name='login'),
 ]
 
 # django debug toolbar
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, render_to_response
 from django.views.generic import CreateView, ListView, TemplateView
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 
 class Usuario(TemplateView):
-    template_name = '../templates/pages/base.html'
+    template_name = 'base.html'
 
     # def get(self, request, *args, **kwargs):
     #     return render(request, self.template_name)
 	
+class Auth(object):
+	def login(request):
+		if request.method == 'POST':
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email, password=password)
+
+			if user and user.is_active:
+				auth_login(request, user)
+				return render(request, "index.html")
+
+		return render(request, "login.html")
